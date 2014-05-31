4487c3ad3d3d1aa504b98f97dace04ca390066a5
Mehedi <mehedi@mehedi-HP-ProBook-450-G1.(none)>
2014-05-31 19:50:29 +0600

    first commit

---
+++ KoNMAC.cpp
@@ -0,0 +1,15 @@
+# include <stdio.h>
+struct sk
+{
+	int cid;
+	int cw;
+};
+
+int ns=7;
+int main()
+{
+
+	
+return 0;
+
+}
---
+++ KoN_MAC.cc
@@ -0,0 +1,24 @@
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/simulator.h"
+#include "ns3/system-condition.h"
+#include "ns3/wifi-phy.h"
+#include "ns3/wifi-module.h"
+#include "ns3/energy-module.h"
+#include "ns3/mobility-module.h"
+
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <algorithm>
+#include <math.h>
